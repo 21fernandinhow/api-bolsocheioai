9953f3194b721c7f5c43155a161b0023b34f9a97
Fernando <21fernandinhow@gmail.com>
2023-10-25 08:12:41 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+const port = process.env.port || 8000;
+
+app.get("/", (req, res) => {
+  res.send("hello world o/");
+});
+
+app.listen(port, () => {
+  console.log("Servidor iniciado...");
+});
