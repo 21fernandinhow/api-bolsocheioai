631e971ccd83895535272d684f1910e97ddb2a38
Fernando <21fernandinhow@gmail.com>
2023-10-26 18:03:03 -0300

    remove build directory

--- build/models/post.d.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Document } from 'mongoose';
-export interface interfacePost extends Document {
-    title: string;
-    date: Date;
-    content: string;
-}
-export declare const PostModel: mongoose.Model<interfacePost, {}, {}, {}, mongoose.Document<unknown, {}, interfacePost> & interfacePost & {
-    _id: mongoose.Types.ObjectId;
-}, any>;
--- build/routes/leadsRouter.d.ts
+++
@@ -1,2 +0,0 @@
-declare const leadsRouter: import("express-serve-static-core").Router;
-export default leadsRouter;
