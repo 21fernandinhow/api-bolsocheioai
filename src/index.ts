9fa783e50fc52c60b0d82afaebf6485f43396463
Fernando <21fernandinhow@gmail.com>
2023-10-27 19:49:28 -0300

    fix cors problems

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,7 @@ function basicAuth(req: Request, res: Response, next: NextFunction){
 //CORS Config
 function corsConfig(req: Request, res: Response, next: NextFunction) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Credentials', 'true');
   
@@ -40,9 +41,9 @@ function corsConfig(req: Request, res: Response, next: NextFunction) {
 //Middleares
 app.use(express.json());
 app.use(corsConfig);
-app.use(basicAuth)
-app.use('/posts', postsRouter)
-app.use('/leads', leadsRouter)
+app.use(basicAuth);
+app.use('/posts', postsRouter);
+app.use('/leads', leadsRouter);
 
 //Server
 app.listen(port, () => {
