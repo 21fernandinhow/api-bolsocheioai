21be180191a028382ec5393d26ea6d759dea104f
Fernando <21fernandinhow@gmail.com>
2023-11-06 17:51:36 -0300

    changed cron jobs time to re execute the cron job

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.use('/posts', postsRouter);
 app.use('/leads', leadsRouter);
 
 //CronJobs
-cron.schedule('0 11 * * 1', async () => {
+cron.schedule('55 17 * * 1', async () => {
   console.log('Executando a função de criar post...');
   const post = await CreatePost();
   sendNewsletter(post);
