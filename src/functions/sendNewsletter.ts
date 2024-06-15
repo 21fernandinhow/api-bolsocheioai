d74389fadc36da9c2ab0de0e5d76891aed4929f3
21fernandinhow <21fernandinhow@gmail.com>
2024-06-15 16:33:22 -0300

    changed newsletter email template: add a unsubscribe option

--- src/functions/sendNewsletter.ts
+++ src/functions/sendNewsletter.ts
@@ -12,15 +12,23 @@ export default async function sendNewsletter(post: interfacePost){
         const htmlBody = `
         <h1> Esse conteúdo com certeza vai te interessar! </h1> 
         <h2 style="color: #1D7044; margin: 20px 0px;"> ${post.title} </h2> 
+
         <a href="https://bolsocheio.ai/posts/${post._id}" 
         style="background: #1D7044; color: #fff; font-weight: bolder; padding: 10px 20px; border-radius: 15px; 
         border: none; text-decoration: none; margin: 10px 0px;">
-        Acesse agora!
+            Acesse agora!
         </a> <br><br>
+
         <hr>
+        
         <h4>Obrigado por acompanhar nossa Newsletter! </h4>
         <img src="https://bolsocheio.ai/_next/image?url=%2Flogo.png&w=128&q=75" width="120" height="80" alt="bolsocheioai">
-        <h4>Bolso Cheio A.I </h4>`
+        <h4>Bolso Cheio A.I </h4>
+
+        <a href="https://bolsocheio.ai/unsubscribe" style="font-size: 0.75rem; margin-top: 10px; color: #1D7044;">
+            Caso deseje cancelar sua inscrição e não receber mais nossos emails, clique aqui.
+        </a>
+        `
         leadsEmails.map(leadEmail => {
             const mailOptionsParameter: mailOptions = {
                 from: "newsletter@bolsocheio.ai",
